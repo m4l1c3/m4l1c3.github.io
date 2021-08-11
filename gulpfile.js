42d7e2e429c2ab8668327d0b56082885149edb11
m4l1c3 <m4l1c3@protonmail.com>
2021-08-10 21:14:13 -0600

    zola init and cleanup

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp'),
-    bundler = require('gulp-bundle-files'),
-    bundles = require('./bundles.json');
-
-gulp.task('bundle', function() {
-    bundler(bundles);
-});
-
-gulp.task('default', ['bundle']);
